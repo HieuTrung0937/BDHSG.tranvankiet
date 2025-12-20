be3e46b3c15dc52f026bffc00c6da95044d40f61
HieuTrung0937 <hieunedep@gmail.com>
2025-12-20 14:11:58 +0700

    Implement Student and TimelineSystem classes
    Added Student and TimelineSystem classes to manage student scans and determine the best student based on reading time.

--- testDE.cpp/BAI_ONLINE/Đề/Đề Long An/BAI2.cpp
+++ testDE.cpp/BAI_ONLINE/Đề/Đề Long An/BAI2.cpp
@@ -1,7 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define TASK "BAI2"
+class Student {
+private:
+    long long id;
+    int firstPos;
+    int lastPos;
 
+public:
+    Student(long long _id, int pos)
+        : id(_id), firstPos(pos), lastPos(pos) {}
+
+    void update(int pos) {
+        lastPos = pos;
+    }
+
+    long long getId() const {
+        return id;
+    }
+
+    int readingTime() const {
+        return lastPos - firstPos;
+    }
+
+    int getFirstPos() const {
+        return firstPos;
+    }
+}; 
+class TimelineSystem {
+private:
+    unordered_map<long long, Student> mp;
+
+public:
+    void addScan(long long id, int time) {
+        if (mp.find(id) == mp.end()) {
+            mp.emplace(id, Student(id, time));
+        } else {
+            mp[id].update(time);
+        }
+    }
+
+    pair<long long, int> getBestStudent() {
+        long long bestId = -1;
+        int bestTime = -1;
+        int bestFirstPos = INT_MAX;
+
+        for (auto &p : mp) {
+            Student &st = p.second;
+            int t = st.readingTime();
+
+            if (t > bestTime ||
+                (t == bestTime && st.getFirstPos() < bestFirstPos)) {
+                bestTime = t;
+                bestId = st.getId();
+                bestFirstPos = st.getFirstPos();
+            }
+        }
+
+        return {bestId, bestTime};
+    }
+};
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
