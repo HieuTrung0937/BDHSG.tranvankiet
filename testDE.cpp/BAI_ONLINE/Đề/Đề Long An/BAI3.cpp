c11ca180becd6e8033835e8c020e830b79b393ea
hieu <emailforwork@gmail.com>
2025-12-20 16:09:08 +0700

    Bai3_1

--- testDE.cpp/BAI_ONLINE/Đề/Đề Long An/BAI3.cpp
+++ testDE.cpp/BAI_ONLINE/Đề/Đề Long An/BAI3.cpp
@@ -1,43 +1,34 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
-
+#define TASK "BAI3"
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
+    cin.tie(nullptr); cout.tie(nullptr);
+    freopen(TASK".inp", "r", stdin);
+    freopen(TASK".out", "w", stdout);
     string N;
     cin >> N;
     int n = N.size();
-
-    // precompute powers
     vector<long long> pow10(n + 1, 1), pow2(n + 1, 1);
     for (int i = 1; i <= n; i++) {
         pow10[i] = pow10[i - 1] * 10;
         pow2[i] = pow2[i - 1] * 2;
     }
-
     long long total = 0;
-
     for (int i = 0; i < n; i++) {
         int digit = N[i] - '0';
 
         long long contrib = 0;
         for (int r = 0; r <= n - i - 1; r++) {
-            // C(n-i-1, r) * 10^r
             long long comb = 1;
             for (int k = 0; k < r; k++)
                 comb = comb * (n - i - 1 - k) / (k + 1);
 
             contrib += comb * pow10[r];
         }
-
         total += digit * contrib * pow2[i];
     }
-
-    // trừ trường hợp không xóa chữ nào
     long long original = 0;
     for (char c : N) original = original * 10 + (c - '0');
 
